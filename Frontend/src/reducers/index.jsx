e67a093d4f4322c40b792c441f027d3398283d32
jsr029 <jsr059@gmail.com>
2022-07-30 12:47:26 +0200

    redux first aplly

--- Frontend/src/reducers/index.jsx
+++ Frontend/src/reducers/index.jsx
@@ -1,11 +1,9 @@
 import counterReducer from "./counter";
 import loggedReducer from "./isLogged";
 import { combineReducers } from "redux";
-import postReducer from "./postReducer";
 
 const allReducers = combineReducers({
     counter: counterReducer,
     isLogged: loggedReducer,
-    posts: postReducer,
 })
 export default allReducers
